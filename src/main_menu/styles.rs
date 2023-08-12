61dd39d0ba97f881f9228b13e365884e58a5390d
Raiyan Hossain <raiyan.hossain@chaldal.net>
2023-08-12 23:39:05 +0600

    Get ui running

--- src/main_menu/styles.rs
+++ src/main_menu/styles.rs
@@ -1,1 +1,11 @@
+use bevy::prelude::*;
 
+pub const MENU_NORMAL_BUTTON_COLOR: Color = Color::DARK_GRAY;
+pub const MENU_BUTTON_SIZE: Size = Size::new(Val::Px(200.), Val::Px(80.));
+
+pub const MENU_BUTTON_STYLE: Style = Style {
+    justify_content: JustifyContent::Center,
+    align_items: AlignItems::Center,
+    size: MENU_BUTTON_SIZE,
+    ..Style::DEFAULT
+};
--- src/main_menu/systems/layout.rs
+++ src/main_menu/systems/layout.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
 
-use crate::main_menu::{self, components::MainMenu};
+use crate::main_menu::{
+    self,
+    components::{MainMenu, PlayButton, QuitButton},
+    styles::*,
+};
 
 pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     let main_menu_entity = build_main_menu(&mut commands, &asset_server);
@@ -18,12 +22,88 @@ pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>)
             MainMenu {},
             NodeBundle {
                 style: Style {
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
                     size: Size::new(Val::Percent(100.), Val::Percent(100.)),
+                    gap: Size::new(Val::Px(8.), Val::Px(8.)),
                     ..default()
                 },
-                background_color: Color::RED.into(),
+                background_color: Color::rgba(0., 0., 0., 0.2).into(),
                 ..default()
             },
         ))
+        .with_children(|parent| {
+            //===Title===
+            parent.spawn(TextBundle {
+                text: Text {
+                    sections: vec![TextSection::new(
+                        "Bevy Ball Game",
+                        TextStyle {
+                            font_size: 32.0,
+                            color: Color::WHITE,
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        },
+                    )],
+                    alignment: TextAlignment::Center,
+                    ..default()
+                },
+                ..default()
+            });
+            //===Play Button===
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: MENU_BUTTON_STYLE,
+                        background_color: MENU_NORMAL_BUTTON_COLOR.into(),
+                        ..default()
+                    },
+                    PlayButton {},
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Play",
+                                TextStyle {
+                                    font_size: 32.0,
+                                    color: Color::WHITE,
+                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                },
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+            //===Quit Button===
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: MENU_BUTTON_STYLE,
+                        background_color: MENU_NORMAL_BUTTON_COLOR.into(),
+                        ..default()
+                    },
+                    QuitButton {},
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Quit",
+                                TextStyle {
+                                    font_size: 32.0,
+                                    color: Color::WHITE,
+                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                },
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+        })
         .id()
 }
